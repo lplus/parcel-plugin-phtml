57fc5468144cbe4dbc6a23a491b032ae23f0bdb3
李云兴 <liyunxing@lingxi360.com>
2018-07-13 22:36:23 +0800

    publish

---
+++ PHTMLAsset.js
@@ -0,0 +1,18 @@
+const HTMLAsset = require("parcel-bundler/src/assets/HTMLAsset");
+const render = require('posthtml-render');
+
+class PHTMLAsset extends HTMLAsset {
+	constructor(name, options) {
+		super(name, options);
+		this.type = 'js';
+	}
+	
+	generate() {
+		const exports = JSON.stringify(
+			this.isAstDirty ? render(this.ast) : this.contents
+		);
+		return `module.exports = ${exports};`;
+	}
+}
+
+module.exports = PHTMLAsset;
